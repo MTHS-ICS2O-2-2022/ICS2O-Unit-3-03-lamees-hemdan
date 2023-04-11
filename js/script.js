82ecd6b8659b4346f4650a5b94cd25942f5048d6
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-04-11 17:16:10 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+
+// This function calculates the volume of a sphere
+
+
+function volumeOfSphere () {
+ // input
+  const radius = parseInt(document.getElementById('radius').value)
+
+
+ // process
+  const volume = 4/3 * Math.PI * Math.Pow(radius, 3)
+
+
+ // output
+  document.getElementById('volume').innerHTML = 'The Volume of Sphere is ' + volume + ' cm³'
+}
